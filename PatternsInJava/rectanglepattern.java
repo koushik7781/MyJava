dba82dde812c74abcc08a31a98c5b759f9f34311
koushik <99732563+koushik7781@users.noreply.github.com>
2024-01-25 18:19:49 +0530

    Add files via upload

---
+++ PatternsInJava/rectanglepattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class rectanglepattern {
+    public static void main(String[] args) {
+        Scanner a=new Scanner(System.in);
+
+        for(int i=1;i<=10;i++)
+        {
+            for(int j=1;j<=20;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+         a.close();
+    }
+}
