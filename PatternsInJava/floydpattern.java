6f987dfc9e7eec0a68ebd821e3cb3c4cd0449a74
koushik <99732563+koushik7781@users.noreply.github.com>
2024-03-01 19:55:33 +0530

    Update floydpattern.java

--- PatternsInJava/floydpattern.java
+++ PatternsInJava/floydpattern.java
@@ -1,11 +1,11 @@
 public class floydpattern {
     public static void main(String[] args) {
         int n=4;
-        int Number=1;
+        int Num=1;
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=i;j++){
-                System.out.print(Number);
+                System.out.print(Num);
                 Number++;
             }
             System.out.println();
